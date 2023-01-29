8d9ac3bcb9d310545b4397a4290fbc871e4edbab
sephtasm <sephtasm@gmail.com>
2023-01-29 08:52:57 -0800

    fire, ice, lightning mutations

--- Content.Shared/Genetics/MutationEffects/GiveActionMutationEffect.cs
+++ Content.Shared/Genetics/MutationEffects/GiveActionMutationEffect.cs
@@ -15,31 +15,48 @@ namespace Content.Shared.Genetics.MutationEffects
     public sealed class GiveActionMutationEffect : MutationEffect
     {
 
+        [DataField("entityTargetActions", customTypeSerializer: typeof(PrototypeIdListSerializer<EntityTargetActionPrototype>))]
+        public readonly List<string> EntityTargetActions = new();
+
         [DataField("worldActions", customTypeSerializer: typeof(PrototypeIdListSerializer<WorldTargetActionPrototype>))]
         public readonly List<string> WorldActions = new();
+
+        [DataField("instantActions", customTypeSerializer: typeof(PrototypeIdListSerializer<InstantActionPrototype>))]
+        public readonly List<string> InstantActions = new();
 
         public override void Apply(EntityUid uid, IEntityManager entityManager, IPrototypeManager prototypeManager)
         {
             var actionsSystem = entityManager.System<SharedActionsSystem>();
-            var actionTypes = new List<ActionType>();
-            foreach (var action in WorldActions)
-            {
-                var actionType = new WorldTargetAction(prototypeManager.Index<WorldTargetActionPrototype>(action));
-                actionTypes.Add(actionType);
-            }
+            var actionTypes = GetAllActions(entityManager, prototypeManager);
             actionsSystem.AddActions(uid, actionTypes, null);
         }
 
         public override void Remove(EntityUid uid, IEntityManager entityManager, IPrototypeManager prototypeManager)
         {
             var actionsSystem = entityManager.System<SharedActionsSystem>();
+            var actionTypes = GetAllActions(entityManager, prototypeManager);
+            actionsSystem.RemoveActions(uid, actionTypes, null);
+        }
+
+        private List<ActionType> GetAllActions(IEntityManager entityManager, IPrototypeManager prototypeManager)
+        {
             var actionTypes = new List<ActionType>();
+            foreach (var action in EntityTargetActions)
+            {
+                var actionType = new EntityTargetAction(prototypeManager.Index<EntityTargetActionPrototype>(action));
+                actionTypes.Add(actionType);
+            }
             foreach (var action in WorldActions)
             {
                 var actionType = new WorldTargetAction(prototypeManager.Index<WorldTargetActionPrototype>(action));
                 actionTypes.Add(actionType);
             }
-            actionsSystem.RemoveActions(uid, actionTypes, null);
+            foreach (var action in InstantActions)
+            {
+                var actionType = new InstantAction(prototypeManager.Index<InstantActionPrototype>(action));
+                actionTypes.Add(actionType);
+            }
+            return actionTypes;
         }
     }
 
--- Resources/Locale/en-US/genetics/components/mutations.ftl
+++ Resources/Locale/en-US/genetics/components/mutations.ftl
@@ -12,4 +12,18 @@ action-description-spell-pyrokinetic-burst = Launch a fiery projectile.
 action-name-spell-pyrokinetic-blast = Pyrokinetic Blast
 action-description-spell-pyrokinetic-blast = Launch a large fiery projectile.
 
+action-name-spell-pyrokinetic-nova = Pyrokinetic Nova
+action-description-spell-pyrokinetic-nova = Manifest a large explosion.
+
+genetics-mutation-name-cryokinesis = Cryokinesis
+action-name-spell-icewall = Ice Wall
+action-description-spell-icewall = Create a wall of ice.
+action-name-spell-ground-ice = Ice Patch
+action-description-spell-ground-ice = Create a patch of ice on the ground.
+
+genetics-mutation-name-electrokinesis = Electrokinesis
+
+action-name-spell-shoot-lightning = Lightning
+action-description-spell-shoot-lightning = Fire a bolt of lightning.
+
 genetics-mutation-name-blindness = Blindness
